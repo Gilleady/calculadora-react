5fd144204a06ba2cbc64e3b8eee103250c98b8cb
Gilleady Alves da Silva <79852665+Gilleady@users.noreply.github.com>
2024-09-16 14:57:58 +0000

    fix: fixed deploy configurations

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,4 +9,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
   },
+  build: {
+    outDir: 'docs', // Define a pasta de saída para o build
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // Define o arquivo de entrada
+    }
+  }
 })
